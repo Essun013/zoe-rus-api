996e443aef67dab08510ce2f8c59121fee27b9ee
heisedebaise <heisedebaise@hotmail.com>
2016-09-09 11:26:41 +0800

    知识库功能开发。

--- zoe-rus-api-kb/src/main/java/com/zoe/rus/kb/knowledge/KnowledgeServiceImpl.java
+++ zoe-rus-api-kb/src/main/java/com/zoe/rus/kb/knowledge/KnowledgeServiceImpl.java
@@ -1,10 +1,14 @@
 package com.zoe.rus.kb.knowledge;
 
+import com.zoe.commons.scheduler.MinuteJob;
 import org.springframework.stereotype.Service;
 
 /**
  * @author lpw
  */
 @Service(KnowledgeModel.NAME + ".service")
-public class KnowledgeServiceImpl implements KnowledgeService {
+public class KnowledgeServiceImpl implements KnowledgeService, MinuteJob {
+    @Override
+    public void executeMinuteJob() {
+    }
 }
